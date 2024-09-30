4babaf0672ba26931a5e053e37289f040a0f2a73
Marganotvke <russechan5-c@my.cityu.edu.com>
2024-09-30 15:42:44 +0800

    fix typo

--- app/modules/knn.js
+++ app/modules/knn.js
@@ -6,25 +6,28 @@ function dotp(x, y) {
       return x[i] * y[i];
     }
     return x.map(dotp_times).reduce(dotp_sum, 0);
-  }  
-  function cosineSimilarity(A,B){
+}
+
+function cosineSimilarity(A,B){
     var similarity = dotp(A, B) / (Math.sqrt(dotp(A,A)) * Math.sqrt(dotp(B,B)));
     return similarity;
+}
+
+function findNearestNeighbors(dataset, targetVector, k = 2, method = 'cosine') {
+  const neighbors = [];
+  let maxSimilarity = -1;
+
+  for (let i = 0; i < dataset.length; i++) {
+    const similarity = cosineSimilarity(targetVector, dataset[i]);
+    if (similarity > maxSimilarity) {
+      neighbors.push(i);
+      maxSimilarity = similarity;
+    }
+
+    if (neighbors.length >= k) {
+      break;
+    }
   }
 
-function findNear(dataset, target, k=2, method='cosine') {
-    var nn = [];
-    var maxSim = -1;
-    var simRes;
-    for (var i = 0; i < dataset.length; i++) {
-        simRes = cosineSimilarity(target, dataset[i]);
-        if (simRes >= maxSim) {
-            nn.push(i);
-            maxSim = simRes;
-        }
-        if (nn.length >= k) {
-            break;
-        }
-    }
-    return nn;
+  return neighbors;
 }
